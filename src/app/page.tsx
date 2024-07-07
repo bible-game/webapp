324f869d25b456b440f5c9ab4c944160a1324eba
jrsmth <jamesrichardsmith97@gmail.com>
2024-07-07 19:08:49 +0100

    [BLE-8] Update date format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ async function getPassage(passageId: string) {
 }
 
 export default async function Home() {
-    const today = new Date().getDate().toString();
-    const passageId = passages[today];
+    const today = new Date();
+    const passageId = passages[today.getDate().toString()];
     const passage = (await getPassage(passageId))["text"];
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>The passage for {today} is:</h1>
+            <h1>The passage for {today.toLocaleDateString()} is:</h1>
             <p>{passage}</p>
         </main>
     );
