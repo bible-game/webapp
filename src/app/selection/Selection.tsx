7d6cffc5dbdd41fdc4b538fb4568d89848f9d812
jrsmth <jamesrichardsmith97@gmail.com>
2024-10-31 10:06:03 +0000

    BLE-22: select chapter

--- src/app/selection/Selection.tsx
+++ src/app/selection/Selection.tsx
@@ -7,18 +7,28 @@ import styles from "./Selection.module.sass"
 // @ts-ignore
 const Selection = () => {
     const [book, setBook] = React.useState('Book');
+    const [chapter, setChapter] = React.useState('Chapter');
+    const [chapters, setChapters] = React.useState([]);
 
-    let selectedChapter = 'Chapter';
     const bible: any[] = [
         { key: "genesis", label: "Genesis", chapters: 50 },
         { key: "exodus", label: "Exodus", chapters: 40 },
         { key: "leviticus", label: "Leviticus", chapters: 27 },
         { key: "numbers", label: "Numbers", chapters: 36 },
         { key: "deuteronomy", label: "Deuteronomy", chapters: 34 },
+
+        { key: "mark", label: "Mark", chapters: 16 },
     ];
 
+    function populateChapters(total: number): void {
+        const chapters = [];
+        for (let i = 1; i <= total; i++)
+            chapters.push({ key: i, label: i.toString() });
+        setChapters(chapters);
+    }
+
     return (
-        <section id={styles.selection}>
+        <section id={styles.selection} className="flex justify-center">
             <Dropdown>
                 <DropdownTrigger>
                     <Button variant="bordered">{book}</Button>
@@ -27,10 +37,12 @@ const Selection = () => {
                     aria-label="Dynamic Actions"
                     items={bible}
                     onAction={(key: string) => {
-                        setBook(bible.find(b => b.key === key).label);
-                        setChapters... // todo
-                    }
-                    className="p-3"
+                        const book = bible.find(b => b.key === key);
+                        populateChapters(book.chapters);
+                        setChapter('Chapter')
+                        setBook(book.label);
+                    }}
+                    className="mt-2 p-3 max-h-[50vh] overflow-y-auto"
                     itemClasses={{
                         base: [
                             "rounded-md",
@@ -43,8 +55,7 @@ const Selection = () => {
                             "data-[pressed=true]:opacity-70",
                             "data-[focus-visible=true]:ring-default-500",
                         ]
-                    }}
-                >
+                    }}>
                     {(item) => (
                         <DropdownItem key={item.key}>{item.label}</DropdownItem>
                     )}
@@ -52,12 +63,16 @@ const Selection = () => {
             </Dropdown>
             <Dropdown>
                 <DropdownTrigger>
-                    <Button variant="bordered">{selectedChapter}</Button>
+                    <Button variant="bordered">{chapter}</Button>
                 </DropdownTrigger>
                 <DropdownMenu
                     aria-label="Dynamic Actions"
-                    items={bible}
-                    className="p-3"
+                    items={chapters}
+                    onAction={(key: string) => {
+                        setChapter(key);
+                        // TODO :: emit answer() event... handle at top-lvl
+                    }}
+                    className="mt-2 p-3 max-h-[50vh] overflow-y-auto"
                     itemClasses={{
                         base: [
                             "rounded-md",
@@ -70,8 +85,7 @@ const Selection = () => {
                             "data-[pressed=true]:opacity-70",
                             "data-[focus-visible=true]:ring-default-500",
                         ]
-                    }}
-                >
+                    }}>
                     {(item) => (
                         <DropdownItem key={item.key}>{item.label}</DropdownItem>
                     )}
