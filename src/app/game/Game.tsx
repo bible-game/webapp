d64f3e7260016eff68451387757da9717d331cdf
jrsmth <jamesrichardsmith97@gmail.com>
2025-01-21 12:52:28 +0000

    [minor] BLE-39: player guess

--- src/app/game/Game.tsx
+++ src/app/game/Game.tsx
@@ -2,110 +2,72 @@
 
 import React from "react";
 import styles from "./Game.module.sass"
-import {Autocomplete, AutocompleteItem} from "@heroui/autocomplete";
+import { Autocomplete, AutocompleteItem } from "@heroui/autocomplete";
+import { Button } from "@nextui-org/react";
+import { Pagination } from "@heroui/pagination";
 
-// @ts-ignore
-const Game = (props) => {
+const Game = (props: any) => {
+    const [page, setPage] = React.useState(1);
     const [book, setBook] = React.useState('Book');
     const [chapter, setChapter] = React.useState('Chapter');
     const [chapters, setChapters] = React.useState([]);
+    const [bookTitle, setBookTitle] = React.useState('Select book');
+    const [chapterTitle, setChapterTitle] = React.useState('Select chapter');
     const [response, setResponse] = React.useState('Select book');
 
-    const bible: any[] = [
-        {
-            book: "Isaiah", chapters: [
-                {chapter: 1, summary: "1: A rebellious nation", verseStart: 1, verseEnd: 31},
-                {chapter: 2, summary: "2: The mountain of the Lord", verseStart: 1, verseEnd: 5},
-                {chapter: 2, summary: "2: The year of the Lord", verseStart: 6, verseEnd: 22}
-            ]
-        },
-        {
-            book: "Acts", chapters: [
-                {chapter: 1, summary: "1: Jesus taken up into heaven", verseStart: 1, verseEnd: 11},
-                {chapter: 1, summary: "1: Mathias chosen to replace Judas", verseStart: 12, verseEnd: 26},
-            ]
-        },
-    ]
+    const bible: any[] = props.bible;
 
     function check(answer: any): void {
-        console.log(`${props.book}${props.chapter}: ${props.summary}`);
-        console.log(answer)
-        if (`${props.book}${props.chapter}: ${props.summary}`.toLocaleLowerCase() == answer.toLocaleLowerCase()) {
+        if (`${props.book}${props.title}`.toLocaleLowerCase() == answer.toLocaleLowerCase()) {
             setResponse('Correct 🎉');
         } else {
             setResponse('Incorrect');
         }
     }
 
-    return (
-        <main>
-            <section id="passage">
-                <h1>{props.today}</h1>
-                <p>{props.passage}</p>
-            </section>
-            <section id={styles.selection} className="flex justify-center">
-                <Autocomplete
-                    className="max-w-xs"
-                    defaultItems={bible}
-                    label="Select book"
-                    onSelectionChange={(key: any) => {
-                        const book = bible.find(b => b.book === key);
-                        setChapters(book?.chapters);
-                        setChapter('Chapter')
-                        setBook(key);
-                        setResponse('Select chapter');
-                    }}
-                    variant="bordered">
-                    {(item: any) => <AutocompleteItem key={item.book}>{item.book}</AutocompleteItem>}
-                </Autocomplete>
-                <Autocomplete
-                    className="max-w-xs"
-                    defaultItems={chapters}
-                    label="Select chaper"
-                    onSelectionChange={(key: any) => {
-                        setChapter(key);
-                        check(book+key)
-                    }}
-                    variant="bordered">
-                    {(item: any) => <AutocompleteItem key={item.summary}>{item.summary}</AutocompleteItem>}
-                </Autocomplete>
-
-                {/*<Dropdown>*/}
-                {/*    <DropdownTrigger>*/}
-                {/*        <Button variant="bordered">{chapter}</Button>*/}
-                {/*    </DropdownTrigger>*/}
-                {/*    <DropdownMenu*/}
-                {/*        aria-label="Dynamic Actions"*/}
-                {/*        items={chapters}*/}
-                {/*        onAction={async (key: any) => {*/}
-                {/*            setChapter(key);*/}
-                {/*            check(book+key)*/}
-                {/*        }}*/}
-                {/*        className="mt-2 p-3 max-h-[50vh] overflow-y-auto"*/}
-                {/*        itemClasses={{*/}
-                {/*            base: [*/}
-                {/*                "rounded-md",*/}
-                {/*                "text-default-500",*/}
-                {/*                "transition-opacity",*/}
-                {/*                "data-[hover=true]:text-foreground",*/}
-                {/*                "data-[hover=true]:bg-default-100",*/}
-                {/*                "dark:data-[hover=true]:bg-default-50",*/}
-                {/*                "data-[selectable=true]:focus:bg-default-50",*/}
-                {/*                "data-[pressed=true]:opacity-70",*/}
-                {/*                "data-[focus-visible=true]:ring-default-500",*/}
-                {/*            ]*/}
-                {/*        }}>*/}
-                {/*        {(item: any) => (*/}
-                {/*            <DropdownItem key={item.key}>{item.label}</DropdownItem>*/}
-                {/*        )}*/}
-                {/*    </DropdownMenu>*/}
-                {/*</Dropdown>*/}
-            </section>
-            <section id={styles.answer} className="flex justify-center mt-8">
-                <p>{response}</p>
-            </section>
-        </main>
-    )
+    return <main>
+        <section id="passage">
+            <h1>{props.today}</h1>
+            <div className="panel">
+                <p>{props.passage.get(page)}</p>
+                <Pagination id={styles.pagination} size="sm" initialPage={page} total={props.passage.size} onChange={(page: number) => setPage(page)}/>
+            </div>
+        </section>
+        <section id={styles.selection} className="flex justify-center">
+        <Autocomplete
+                className="max-w-sm"
+                defaultItems={bible}
+                label={bookTitle}
+                onSelectionChange={(key: any) => {
+                    const book = bible.find(b => b.book === key);
+                    setChapters(book?.chapters);
+                    setChapter('Chapter')
+                    setBook(key);
+                    setResponse('Select chapter');
+                    setBookTitle('The Law, Old Testament')
+                }}
+                variant="bordered">
+                {(item: any) => <AutocompleteItem key={item.book}>{item.book}</AutocompleteItem>}
+            </Autocomplete>
+            <Autocomplete
+                className="max-w-sm"
+                defaultItems={chapters}
+                label={chapterTitle}
+                onSelectionChange={(key: any) => { setChapter(key); setChapterTitle('Chapter ' + (chapters as any).find((c: any) => c.title === key).chapter) }}
+                variant="bordered">
+                {(item: any) => <AutocompleteItem key={item.title}>{item.title}</AutocompleteItem>}
+            </Autocomplete>
+            <Button
+                id={styles.guess}
+                onClick={() => { check(book+chapter) }}
+                variant="bordered">
+                Guess
+            </Button>
+        </section>
+        <section id={styles.answer} className="flex justify-center mt-8">
+            <p>{response}</p>
+        </section>
+    </main>
 }
 
 export default Game;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import moment from "moment";
 import Game from "@/app/game/Game";
+import { promises as fs } from 'fs';
 
 async function getPassage() {
     const response = await fetch(`${process.env.passageService}/daily`, {
@@ -12,7 +13,39 @@ async function getPassage() {
     return response.json();
 }
 
+function paginate(passage: string) {
+    const remaining = (segment: string) => {
+        const sentences = segment.split(".");
+        return sentences[sentences.length - 1];
+    }
+
+    const pages = new Map();
+    const segments = passage.match(/[\s\S]{1,1000}/g)!!
+    let head = ""; let tail = "";
+
+    for (let i = 0; i < segments.length; i++) {
+        const segment = segments[i];
+        const lastCharacter = segment.substring(segment.length - 1);
+
+        const last: boolean = i == segments.length - 1;
+        tail = last ? "" : lastCharacter == "." ? ".." : "...";
+
+        const page = i + 1;
+        const content = `${head}${segment}${tail}`
+        pages.set(page, content);
+
+        const first: boolean = i == 0;
+        head = first ? "" : lastCharacter == "." ? "" : remaining(segment);
+        tail = "";
+    }
+
+    return pages
+}
+
 export default async function App() {
+    const file: any = await fs.readFile(process.cwd() + '/../deployment/configuration/bible.json', 'utf8');
+    const bible: any = JSON.parse(file);
+
     const today = moment(new Date()).format('dddd Do MMMM YYYY');
     const passage = await getPassage();
 
@@ -21,8 +54,9 @@ export default async function App() {
             today={today}
             book={passage['book']}
             chapter={passage['chapter']}
-            summary={passage['summary']}
-            passage={passage['text']}
+            title={passage['title']}
+            passage={paginate(passage['text'])}
+            bible={bible.books}
         />
     );
 }
