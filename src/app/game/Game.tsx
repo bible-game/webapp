93d6ffa49a4c04fa3dafa6b165135bdb02f97afe
jrsmth <jamesrichardsmith97@gmail.com>
2025-01-31 15:49:44 +0000

    [minor] BLE-69: global dropdown and back-fill

--- src/app/game/Game.tsx
+++ src/app/game/Game.tsx
@@ -16,24 +16,44 @@ const Game = (props: any) => {
                 .then((response) => {
                     response.json().then((data) => setPassage(data));
                 });
+
+            flattenDivisions();
+            flattenBooks();
         }
     })
 
     const [passage, setPassage] = React.useState({} as any); // question :: apply type?
     const [guesses, setGuesses] = React.useState([] as any[]); // question :: apply type?
-    const [testaments] = React.useState(props.bible.testaments);
+    const [testaments, setTestaments] = React.useState(props.bible.testaments);
     const [divisions, setDivisions] = React.useState([] as any[]);
-    const [books, setBooks] = React.useState([] as any);
+    const [books, setBooks] = React.useState([] as any[]);
     const [chapters, setChapters] = React.useState([] as any);
-    const [selected] = React.useState({} as {testament: string, division: string, book: string, chapter: string});
+    const [selected, setSelected] = React.useState({} as {testament: string, division: string, book: string, chapter: string});
     const [book, setBook] = React.useState(<span className="mr-4 p-2 w-[8rem]">Book?</span>);
     const [chapter, setChapter] = React.useState(<span className="p-2 w-[8rem]">Chapter?</span>);
 
     function selectTestament(item: string): void {
         selected.testament = item;
+
         setDivisions(testaments.find(
             (test: any) => test.name === item
         ).divisions);
+    }
+
+    function flattenDivisions() {
+        const divisions = [];
+        for (const test of testaments) {
+            for (const div of test.divisions) divisions.push(div);
+        }
+        setDivisions(divisions);
+    }
+
+    function flattenBooks() {
+        const books = [];
+        for (const div of divisions) {
+            for (const book of div.books) books.push(book);
+        }
+        setBooks(books);
     }
 
     function selectDivision(item: string): void {
@@ -42,6 +62,9 @@ const Game = (props: any) => {
         setBooks(divisions!!.find(
             (div: any) => div.name === item
         ).books);
+
+        const testament = testaments.find((test: any) => test.divisions.some((div: any) => div.name == item));
+        selectTestament(testament.name);
     }
 
     function selectBook(item: string): void {
@@ -52,6 +75,9 @@ const Game = (props: any) => {
         for (let i = 1; i <= numChapters; i++) chapters.push({ name: i.toString() });
 
         setChapters(chapters);
+
+        const division = divisions.find((div: any) => div.books.some((book: any) => book.name == item));
+        selectDivision(division.name);
     }
 
     function selectChapter(item: string): void {
@@ -75,6 +101,14 @@ const Game = (props: any) => {
         }
     }
 
+    // fixme :: behaviour not correct
+    function clearSelection(): void {
+        selected.testament = '';
+        selected.division = '';
+        selected.book = '';
+        selected.chapter = '';
+    }
+
     return <main>
         <section>
             <h1>{today}</h1>
@@ -93,7 +127,9 @@ const Game = (props: any) => {
                 inputProps={{classNames: {inputWrapper: "border"}}}
                 defaultItems={testaments}
                 label="Testament"
+                onClear={() => clearSelection()}
                 onSelectionChange={(key: any) => { selectTestament(key) }}
+                selectedKey={selected.testament}
                 variant="bordered">
                 {(item: any) =>
                     <AutocompleteItem className="text-black" key={item.name}>{item.name}</AutocompleteItem>}
@@ -102,7 +138,9 @@ const Game = (props: any) => {
                 className="flex-1"
                 inputProps={{classNames: {inputWrapper: "border"}}}
                 defaultItems={divisions}
+                selectedKey={selected.division}
                 label="Division"
+                onClear={() => clearSelection()}
                 onSelectionChange={(key: any) => { selectDivision(key) }}
                 variant="bordered">
                 {(item: any) =>
@@ -113,6 +151,7 @@ const Game = (props: any) => {
                 inputProps={{classNames: {inputWrapper: "border"}}}
                 defaultItems={books}
                 label="Book"
+                onClear={() => clearSelection()}
                 onSelectionChange={(key: any) => { selectBook(key) }}
                 variant="bordered">
                 {(item: any) =>
@@ -122,7 +161,11 @@ const Game = (props: any) => {
                 className="flex-1"
                 inputProps={{classNames: {inputWrapper: "border"}}}
                 defaultItems={chapters}
+                onClear={() => clearSelection()}
                 onSelectionChange={(key: any) => { selectChapter(key) }}
+                listboxProps={{
+                    emptyContent: "Select a book",
+                }}
                 label="Chapter"
                 variant="bordered">
                 {(item: any) =>
