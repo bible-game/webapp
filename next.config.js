1eb3ea709fadb9c5ad86d7b6d936d1f7852e3e15
jrsmth <jamesrichardsmith97@gmail.com>
2025-02-24 19:58:43 +0000

    [minor] todo

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        SVC_PASSAGE: 'https://passage-oa8a.onrender.com' // 'http://localhost:8081'
+        SVC_PASSAGE: 'https://passage-oa8a.onrender.com' // 'http://localhost:8081' // 'https://passage-oa8a.onrender.com'
+        // TODO :: explore proper environment handling
     },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
