a22f261b54973ff72e31523259aaa795cd508cef
jrsmth <jamesrichardsmith97@gmail.com>
2025-05-03 14:17:05 +0100

    [patch] BLE-104: enable read-any

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -8,13 +8,15 @@ import Link from "next/link";
 export const Navigation = (props: any) => {
     const links = [];
 
-    if (props.play) links.push(<div className="pl-4"><Link href='/play' key='play'><p className="text-blue-400">Play</p></Link></div>);
-    if (props.read) links.push(<div className="pl-4"><Link href='/read/acts' key='read'><p className="text-purple-400">Read</p></Link></div>);
-    if (props.stats) links.push(<div className="pl-4"><Link href='/stats' key='stats'><p className="text-green-400">Stats</p></Link></div>);
+    if (props.play) links.push(<div className="w-[4rem]"><Link href='/play' key='play'><p className="text-blue-400">Play</p></Link></div>);
+    if (props.read) links.push(<div className="w-[4rem]"><Link href='/read/genesis1' key='read'><p className="text-purple-400">Read</p></Link></div>);
+    if (props.stats) links.push(<div className="w-[4rem]"><Link href='/stats' key='stats'><p className="text-green-400">Stats</p></Link></div>);
 
     return (
-        <section className="flex justify-end relative">
-            {links.map((link: any) => link)}
+        <section className="flex absolute top-[4.375rem] w-[38rem] justify-end">
+            <div className="flex justify-end relative">
+                {links.map((link: any) => link)}
+            </div>
         </section>
     )
 }
