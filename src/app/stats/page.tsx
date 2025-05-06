cd45bfd369bd4e39fabdec628afeba9da9ae454f
jrsmth <jamesrichardsmith97@gmail.com>
2025-05-06 16:33:27 +0100

    [patch] BLE-101: style stats pg

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -312,20 +312,20 @@ export default function Stats() {
             <main>
                 <Toaster position="bottom-right"/>
                 <section>
-                    <h1>Statistics</h1>
-                    <div className="flex my-8 justify-start gap-8">
-                        <div className="flex">
-                            <p>{stars}</p>
+                    <p className="text-[1.5rem]">Statistics</p>
+                    <div className="flex my-8 justify-start gap-12 items-center">
+                        <div className="flex items-center">
+                            <p className="text-[1.5rem]">{stars}</p>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24"
                                  stroke-width="1.5"
-                                 stroke="gold" className="size-4">
+                                 stroke="gold" className="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                       d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"/>
                             </svg>
                         </div>
-                        <div><p>{games} games</p></div>
-                        <div><p>{streak} days</p></div>
-                        <div><p>{complete}%</p></div>
+                        <div><p className="text-[1.5rem]">{games}<span className="text-gray-400 text-[1rem] ml-1">games</span></p></div>
+                        <div><p className="text-[1.5rem]">{streak}<span className="text-gray-400 text-[1rem] ml-1">days</span></p></div>
+                        <div><p className="text-[1.5rem]">{complete}<span className="text-gray-400 text-[1rem]">%</span></p></div>
                     </div>
                 </section>
                 <section className="flex flex-wrap">
