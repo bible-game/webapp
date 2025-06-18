a41757ae46a50b043f12a4f7b946e2a30db74b1f
jrsmth <jamesrichardsmith97@gmail.com>
2025-06-19 00:08:02 +0100

    [patch] BLE-118: mobile test (ii)

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,6 @@ import { NextRequest, NextResponse, userAgent } from 'next/server'
 export async function middleware(request: NextRequest) {
     const { device } = userAgent(request);
     const type = device.type ?? 'desktop';
-    console.log("device");
-    console.log(device);
 
     const response = NextResponse.next()
     response.headers.append('x-device-type', type)
