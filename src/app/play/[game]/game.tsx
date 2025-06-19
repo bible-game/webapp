eb9150a87bbfc1b2e4d7d99182394e2ebc6d30b7
jrsmth <jamesrichardsmith97@gmail.com>
2025-06-19 21:48:41 +0100

    [patch] BLE-118: bring treemap to front

--- src/app/play/[game]/game.tsx
+++ src/app/play/[game]/game.tsx
@@ -183,7 +183,6 @@ export default function Game(props: any) {
 
         return (
             <>
-                <Treemap passage={passage} select={select} bookFound={bookFound} divFound={divisionFound} testFound={testamentFound} data={testaments} book={book} device={props.device}/>
                 <section className="relative z-1 h-full pointer-events-none">
                     <section className="pointer-events-auto">
                         {props.device}<Menu passage={passage} playing={playing} date={props.game}/>
@@ -194,6 +193,7 @@ export default function Game(props: any) {
                     </section>
                     <Confetti fire={confetti}/>
                 </section>
+                <Treemap passage={passage} select={select} bookFound={bookFound} divFound={divisionFound} testFound={testamentFound} data={testaments} book={book} device={props.device}/>
             </>
         );
     }
