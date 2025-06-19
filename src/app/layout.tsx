ed8428b67882e4c9331da2f4cf571902ab9b7476
jrsmth <jamesrichardsmith97@gmail.com>
2025-06-19 20:47:04 +0100

    [patch] BLE-118: export viewport in <head>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,25 +4,29 @@ import React from "react";
 import Providers from "./providers";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Viewport } from 'next'
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Bible Game",
   description: "A game that explores the Bible 📖✨",
 };
 
-export const viewport: Viewport = {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-    // Also supported but less commonly used
-    // interactiveWidget: 'resizes-visual',
-}
+// export const viewport: Viewport = {
+//     width: 'device-width',
+//     initialScale: 1,
+//     maximumScale: 1,
+//     userScalable: false,
+//     // Also supported but less commonly used
+//     // interactiveWidget: 'resizes-visual',
+// }
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
   return (
       <html lang="en">
           <SpeedInsights/>
+          <Head>
+              <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+          </Head>
           <body>
               <Providers>
                   {children}
