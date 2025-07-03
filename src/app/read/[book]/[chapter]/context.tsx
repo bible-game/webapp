a0fb145f0fbbf3f926566c774a0361a28c6647b3
jrsmth <jamesrichardsmith97@gmail.com>
2025-07-03 19:23:33 +0100

    [patch] BLE-158: mobile context

--- src/app/read/[book]/[chapter]/context.tsx
+++ src/app/read/[book]/[chapter]/context.tsx
@@ -8,7 +8,7 @@ const Context = (props: any) => {
     // TODO :: quality of life -> make accordion colour match the division
     return <Accordion className="bg-secondary-50 p-4 rounded">
         <AccordionItem key="1" aria-label="pre-context" title="Context" className="text-secondary-800">
-            <p className="max-w-[38rem] font-light text-sm leading-6 text-secondary-800">{props.content}</p>
+            <p className="max-w-[16rem] sm:max-w-[38rem] font-light text-sm leading-6 text-secondary-800">{props.content}</p>
         </AccordionItem>
     </Accordion>
 }
