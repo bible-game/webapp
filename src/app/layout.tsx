5e0bab0802c1eaa67db263fa1fc33be4b5dfeabe
jrsmth <jamesrichardsmith97@gmail.com>
2025-07-07 00:51:44 +0100

    [exp] rm treemap pan-xy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
     width: 'device-width',
-    initialScale: 1,
+    initialScale: 1.0,
     userScalable: false,
     viewportFit: 'cover'
     // Also supported but less commonly used
