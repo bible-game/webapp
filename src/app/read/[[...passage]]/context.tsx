be9cd4d2399fc83830aaef23c3d422638bcf2d78
jrsmth <jamesrichardsmith97@gmail.com>
2025-07-10 14:43:12 +0100

    [tweak] tailwind cfg for accordion symbol

--- src/app/read/[[...passage]]/context.tsx
+++ src/app/read/[[...passage]]/context.tsx
@@ -10,6 +10,9 @@ const Context = (props: any) => {
     const [context, setContext] = useState("");
     const [loading, setLoading] = useState(false);
 
+    const [title] = useState(props.context == 'before' ? 'Before' : 'After');
+    const [subtitle] = useState(props.context == 'before' ? `Click for context leading up to ${props.passageKey}` : `Click to understand the events following ${props.passageKey}`);
+
     function toggle(e: any): void {
         if (!context && e.size) {
             setLoading(true);
@@ -23,7 +26,7 @@ const Context = (props: any) => {
 
     // TODO :: quality of life -> make accordion colour match the division?
     return <Accordion className="bg-secondary-50 p-4 rounded" onSelectionChange={toggle}>
-        <AccordionItem key="1" aria-label="pre-context" title="Context" className="text-secondary-800">
+        <AccordionItem key="1" aria-label="pre-context" title={title} subtitle={subtitle} className="text-secondary-800">
             {
                 loading ? <Spinner color="secondary"/> :
                     <p className="max-w-[16rem] sm:max-w-[38rem] font-light text-sm leading-6 text-secondary-800">{context}</p>
