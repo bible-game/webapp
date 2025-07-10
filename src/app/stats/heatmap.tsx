01d069164e62de26743b4f966c4ad2d1a7ba2280
jrsmth <jamesrichardsmith97@gmail.com>
2025-07-10 12:48:14 +0100

    [tweak] stats heatmap key

--- src/app/stats/heatmap.tsx
+++ src/app/stats/heatmap.tsx
@@ -7,9 +7,9 @@ const Heatmap = (props: any) => {
 
     return (
         <section className="flex flex-wrap sm:w-[46rem] w-[80vw] pb-12">
-            {props.data.map((data: any) => <div className="p-0 flex flex-wrap">
+            {props.data.map((data: any) => <div className="p-0 flex flex-wrap" key={data.book}>
                 <p className="font-extralight text-[10px] w-full pb-1 pt-2 opacity-80">{data.book}</p>
-                {data.chapters.map((chapter: any) => <div className="p-[1px] flex flex-wrap">{chapter.verses.map((verse: any, index: number) =>
+                {data.chapters.map((chapter: any) => <div key={data.book+chapter.chapter} className="p-[1px] flex flex-wrap">{chapter.verses.map((verse: any, index: number) =>
                     <Cell key={data.book + chapter.chapter + index} verse={verse}/>)}</div>)}</div>)}
         </section>
     );
