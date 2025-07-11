478d1a09f6a0bb8b156caf4d83cfcd34925a2ee7
jrsmth <jamesrichardsmith97@gmail.com>
2025-07-11 11:31:47 +0100

    [patch] BLE-179: study styling

--- src/app/study/[passage]/page.tsx
+++ src/app/study/[passage]/page.tsx
@@ -18,11 +18,17 @@ export default async function Study({params}: { params: Promise<{ passage: strin
     return (
         <div className="bg-white absolute top-0 left-0 w-full h-full">
             <div className="flex justify-center">
-                <Link href={"/read/"+passage} className="text-black">Back to Reading</Link>
+                <Link href={"/read/" + passage} className="p-4 text-black w-[12rem] flex gap-2 items-center h-min">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.25"
+                         stroke="currentColor" className="size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"/>
+                    </svg>
+                    <span className="font-light">Back to Read</span>
+                </Link>
                 <main
                     className="bg-white h-min w-full text-black relative top-[6rem] overflow-auto pb-[12rem] flex justify-center">
                     <div className="w-full max-w-4xl p-4">
-                        <h1 className="text-2xl font-bold mb-4">{study.passage}</h1>
+                    <h1 className="text-2xl font-bold mb-4">{passage.replace(/[a-z](?=\d)|\d(?=[a-z])/gi, '$& ')}</h1>
                         <p className="mb-8">{study.text}</p>
                         <Questions questions={study.questions} passage={passage} />
                     </div>
