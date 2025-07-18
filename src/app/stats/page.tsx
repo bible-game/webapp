0568788bc4ec3d015263a1c7f88a1c09ec777485
jrsmth <jamesrichardsmith97@gmail.com>
2025-07-18 23:44:19 +0100

    [patch] BLE-123: establish user info

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -8,14 +8,14 @@ import Image from 'next/image'
 import Metrics from "@/app/stats/metrics";
 import Completion from "@/app/stats/completion";
 import LoginPrompt from "@/app/stats/login-prompt";
-import { cookies } from "next/headers";
-import { jwtVerify } from "jose";
+
 import LogoutButton from "@/app/stats/logout-button";
 import { getReadState } from "@/core/action/state/get-state-read";
 import { ReadState } from "@/core/model/state/read-state";
 import { GameState } from "@/core/model/state/game-state";
 import { getGameState } from "@/core/action/state/get-state-game";
 import isLoggedIn from "@/core/util/auth-util";
+import getUserInfo, { UserInfo } from "@/core/action/user/get-user-info";
 
 async function get(url: string): Promise<any> {
     const response = await fetch(url, {method: "GET"});
@@ -28,26 +28,18 @@ async function get(url: string): Promise<any> {
  */
 export default async function Stats() {
 
-    let firstname: string | undefined = undefined;
-    const cookieStore = await cookies();
-
+    let displayName: string | undefined;
     const bible = await get(`${process.env.SVC_PASSAGE}/config/bible`);
 
     let gameState: Map<number,GameState> | undefined;
     let readState: Map<string,ReadState> | undefined;
+    let info: UserInfo | undefined;
     if (await isLoggedIn()) {
         gameState = await getGameState();
         readState = await getReadState();
-    }
 
-    try {
-        const token = cookieStore.get('token');
-        if (token) {
-            const { payload } = await jwtVerify(token.value, new TextEncoder().encode(process.env.SIGNING_SECRET))
-            firstname = payload.sub as string; // todo :: display name, Giles Denham...
-        }
-    } catch (e) {
-        console.error(e)
+        info = await getUserInfo()
+        displayName = `${info?.firstname} ${info?.lastname}`
     }
 
     return (
@@ -58,15 +50,15 @@ export default async function Stats() {
                 <section>
                     <div className="flex gap-12 items-center">
                         <div className="flex gap-2">
+                            {displayName && <h1 className="text-[1.5rem] mx-0">{`${displayName}'s`}</h1>}
                             <h1 className="text-[1.5rem] mx-0">Statistics</h1>
-                            {firstname && <h1 className="text-[1.5rem] mx-0">{`for ${firstname}`}</h1>}
                         </div>
-                        {firstname && <LogoutButton/>}
+                        {info && <LogoutButton/>}
                         <a href="https://discord.gg/6ZJYbQcph5" target="_blank" className="flex gap-2 items-center translate-y-2.5">
                             <Image src="/discord.png" alt="discord" width={100} height={0}/>
                         </a>
                     </div>
-                    {!firstname && <LoginPrompt/>}
+                    {!info && <LoginPrompt/>}
                     <Metrics gameState={gameState} readState={readState}/>
                     <Completion gameState={gameState} readState={readState} bible={bible}/>
                 </section>
