5323c3050e315eb4d23c37fba8a03b0fb1cc40c3
jrsmth <jamesrichardsmith97@gmail.com>
2025-08-10 15:29:14 +0100

    Added Image to Pop-up

--- src/app/play/[game]/pop-up.tsx
+++ src/app/play/[game]/pop-up.tsx
@@ -7,7 +7,8 @@ import {
   Button,
   useDisclosure,
 } from "@heroui/react";
-import {useState} from "react";
+import React, {useState} from "react";
+import Image from "next/image";
 
 export default function PopUp() {
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
@@ -36,38 +37,45 @@ export default function PopUp() {
           {(onClose) => (
             <>
               <ModalHeader className="flex flex-col gap-1">Welcome to The Bible Game</ModalHeader>
-              <ModalBody>
-                <p>
-                  A free web app that encourages Bible Study, committing Scripture to memory and building a Bible ready habit in a fun and stimulating way.
-                </p>
-                <p className="mt-8">
-                  How To Play
-                </p>
-                <ul>
-                    <li>
-                        •	On the web app, there is a daily description of a Bible Chapter.
-                    </li>
-                    <li>
-                        •	You are to use the clues in the description to find where the Bible chapter is located.
-                    </li>
-                    <li>
-                        •	You try to find the verse by selecting the possible Book and Chapter you think/know the clues are pointing to.
-                    </li>
-                    <li>
-                        	You have five guesses per session and with every guess, your feedback will have a number and pointer next to it. 
-                    </li>
-                    <li>
-                        •	The numbers indicate how many words you are away from the correct answer and the pointer indicates what direction the correct might be - either above or below your current guess.
-                    </li>
-                </ul>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={close}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={close}>
-                  Action
-                </Button>
+                <ModalBody>
+                    <p>
+                        A free web app that encourages Bible Study, committing Scripture to memory and building a Bible
+                        ready habit in a fun and stimulating way.
+                    </p>
+                    <div className="flex my-4 justify-center">
+                        <Image src="/mark-1.png" alt="mark1" width={30 * 16} height={0} className="rounded w-[95%]"/>
+                    </div>
+                    <p className="mt-8">
+                        How To Play
+                    </p>
+                    <ul>
+                        <li>
+                            • On the web app, there is a daily description of a Bible Chapter.
+                        </li>
+                        <li>
+                            • You are to use the clues in the description to find where the Bible chapter is located.
+                        </li>
+                        <li>
+                            • You try to find the verse by selecting the possible Book and Chapter you think/know the
+                            clues are pointing to.
+                        </li>
+                        <li>
+                            You have five guesses per session and with every guess, your feedback will have a number and
+                            pointer next to it.
+                        </li>
+                        <li>
+                            • The numbers indicate how many words you are away from the correct answer and the pointer
+                            indicates what direction the correct might be - either above or below your current guess.
+                        </li>
+                    </ul>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="danger" variant="light" onPress={close}>
+                        Close
+                    </Button>
+                    <Button color="primary" onPress={close}>
+                        Action
+                    </Button>
               </ModalFooter>
             </>
           )}
