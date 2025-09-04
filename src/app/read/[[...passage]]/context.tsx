639a6ac2758e8db40ba005ed2bd631de1b6f3211
jrsmth <jamesrichardsmith97@gmail.com>
2025-09-04 12:00:19 +0100

    [patch] BLE-237: general /read styling

--- src/app/read/[[...passage]]/context.tsx
+++ src/app/read/[[...passage]]/context.tsx
@@ -4,35 +4,39 @@ import React, { useState } from "react";
 import { Accordion, AccordionItem, Spinner } from "@heroui/react";
 import { getPostContext } from "@/core/action/read/get-postcontext";
 import { getPreContext } from "@/core/action/read/get-precontext";
+import {InfoCircleIcon} from "@heroui/shared-icons";
 
 const Context = (props: any) => {
-
     const [context, setContext] = useState("");
     const [loading, setLoading] = useState(false);
-
-    const [title] = useState(props.context == 'before' ? 'Pre-Context' : 'Post-Context');
+    const [title] = useState(props.context == "before" ? "Pre‑Context" : "Post‑Context");
 
     function toggle(e: any): void {
         if (!context && e.size) {
             setLoading(true);
-
-            (props.context == 'after' ? getPostContext : getPreContext)(props.passageKey).then((ctx: any) => {
+            (props.context == "after" ? getPostContext : getPreContext)(props.passageKey).then((ctx: any) => {
                 setContext(ctx.text);
                 setLoading(false);
             });
         }
     }
 
-    // TODO :: quality of life -> make accordion colour match the division?
-    return <Accordion className="opacity-80 bg-gradient-to-bl from-blue-50 to-blue-100 p-4 rounded border-1 border-blue-200 text-blue-700 font-light" onSelectionChange={toggle} isCompact={true}>
-        <AccordionItem key="1" aria-label="context" title={title}>
-            {
-                loading ? <Spinner color="primary"/> :
-                    <p className="max-w-[16rem] sm:max-w-[38rem] font-extralight text-sm leading-6 text-gray-700">{context}</p>
-            }
-
-        </AccordionItem>
-    </Accordion>
-}
+    return (
+        <Accordion
+            className="opacity-90 bg-white/80 backdrop-blur-sm ring-1 ring-indigo-500 rounded-2xl p-3 sm:p-4"
+            itemClasses={{title: "text-indigo-700 font-medium", indicator: "text-indigo-700", startContent: "text-indigo-700 bg-red"}}
+            onSelectionChange={toggle}
+            isCompact
+        >
+            <AccordionItem key="1" aria-label="context" title={title}>
+                {loading ? (
+                    <Spinner color="primary" />
+                ) : (
+                    <p className="max-w-[65ch] text-sm leading-6 text-indigo-700">{context}</p>
+                )}
+            </AccordionItem>
+        </Accordion>
+    );
+};
 
 export default Context;
