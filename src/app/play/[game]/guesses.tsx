b231b73473f3378f66f1b55aff0c2bb828204e05
jrsmth <jamesrichardsmith97@gmail.com>
2025-09-11 18:38:47 +0100

    [demo] alignment

--- src/app/play/[game]/guesses.tsx
+++ src/app/play/[game]/guesses.tsx
@@ -6,7 +6,7 @@ import { Star } from "@/app/play/[game]/star";
 
 const Guesses = (props: any) => {
 
-    return <section className="px-4 flex justify-center flex-wrap gap-4 mt-0 sm:mt-4 absolute sm:bottom-[1.5rem] sm:py-0 py-2">
+    return <section className="px-4 flex justify-center flex-wrap gap-4 mt-0 sm:mt-4 absolute sm:bottom-[1.5rem] sm:py-0 py-2 sm:left-[calc(50%-23rem)]">
         {props.guesses.map((guess: any) => <Guess book={guess.bookKey || guess.book} key={guess.book + guess.chapter}
                                             chapter={guess.chapter}
                                             bookFound={props.bookFound}
