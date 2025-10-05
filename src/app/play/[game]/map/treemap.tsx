be8ce43f36cd64135f84a82a5b3740320bc942ab
jrsmth <jamesrichardsmith97@gmail.com>
2025-10-05 16:58:10 +0100

    [patch] BLE-243: continue tuning max draw duration

--- src/app/play/[game]/map/treemap.tsx
+++ src/app/play/[game]/map/treemap.tsx
@@ -135,8 +135,8 @@ const Treemap = (props: any) => {
                 groupMinDiameter: 0,
                 groupStrokeType: 'plain',
                 groupStrokeWidth: 2,
-                finalCompleteDrawMaxDuration: props.device == "mobile" ? 100 : 20000,
-                wireframeDrawMaxDuration: props.device == "mobile" ? 100 : 20000,
+                finalCompleteDrawMaxDuration: props.device == "mobile" ? 135 : 20000,
+                wireframeDrawMaxDuration: props.device == "mobile" ? 135 : 20000,
                 // todo :: continue to tune for mobile (performance vs smooth draw) ^^
                 // finalIncrementalDrawMaxDuration: props.device == "mobile" ? 100 : 20000,
                 groupFillType: 'gradient',
