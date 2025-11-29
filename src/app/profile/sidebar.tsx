4c2b9c223d20f918ca4843b36df6b444f32fd51d
AdamJ335 <adamj335@gmail.com>
2025-11-29 12:24:40 +0000

    [feature] Initialise modal to edit Profile Picture

--- src/app/profile/sidebar.tsx
+++ src/app/profile/sidebar.tsx
@@ -1,8 +1,15 @@
+"use client"
+
 import Image from "next/image";
+import { ModalBody, ModalFooter, ModalHeader } from "@heroui/modal";
+import { Modal, ModalContent, useDisclosure } from "@heroui/react";
+import { Button } from "@heroui/button";
 
 export default function ProfileSidebar({ info }: { info: any }) {
     const fallbackUrl =
         "https://raw.githubusercontent.com/bible-game/webapp/main/public/icon-nobg.png";
+
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
     return (
         <aside className="space-y-4">
@@ -16,6 +23,40 @@ export default function ProfileSidebar({ info }: { info: any }) {
                     alt="Profile"
                     className="rounded-full border"
                 />
+                <Button onPress={onOpen}>Edit</Button>
+                <Modal
+                    backdrop="opaque"
+                       classNames={{
+                           body: "p-8 text-center",
+                           backdrop: "bg-[#060842]/75",
+                           base: "max-w-[40rem] h-min bg-gradient-to-t from-[#0f0a31] to-[#060842] border-[1px] border-[#ffffff]/25",
+                           header: "pt-8 w-full text-center",
+                           closeButton: "hover:bg-white/5 active:bg-white/10 absolute right-6 top-6",
+                       }}
+                       isOpen={isOpen}
+                       radius="lg"
+                       placement="center"
+                       onOpenChange={onOpenChange}>
+                    <ModalContent>
+                        {(onClose) => (
+                            <>
+                                <ModalHeader className="flex flex-col gap-1">Editing Picture</ModalHeader>
+                                <ModalBody>
+                                    <p>Hello!</p>
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button color="danger" variant="light" onPress={onClose}>
+                                        Close
+                                    </Button>
+                                    <Button color="primary" onPress={onClose}>
+                                        Action
+                                    </Button>
+                                </ModalFooter>
+                            </>
+                        )}
+                    </ModalContent>
+
+                </Modal>
             </div>
 
             {/* Name */}
