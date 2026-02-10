f24f167217fcf09ed8ce4167460ef7ca54d5a20f
jrsmth <jamesrichardsmith97@gmail.com>
2026-02-10 08:30:13 +0000

    [test] reorg files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,18 @@ import "./globals.sass";
 import React from "react";
 import Providers from "./providers";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Head from "next/head";
+
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
   return (
       <html lang="en">
           <SpeedInsights/>
-          <Head>
-              <script src="./hammer.min.js" async />
-          </Head>
+
           <body>
           <Providers>
               {children}
             </Providers>
+            <script src="./hammer.min.js" async />
           </body>
       </html>
     );
